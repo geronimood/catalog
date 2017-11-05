ef2cdcdee88fd0941a5319d79fa8c7fdd44f2663
Christoph Zeller <zeller.christoph@gmx.de>
2017-11-05 21:12:15 +0100

    Add route procedures and render templates for showCatalog, newCatalog, editCatalog, deleteCatalog, showCategory and newCategoryItem

--- application.py
+++ application.py
@@ -17,6 +17,10 @@ import requests
 
 app = Flask(__name__)
 
+# Fake User while login is not implemented:
+
+login_session = {'user_id': 2}
+
 # Setting Client ID and application name
 
 #CLIENT_ID = json.loads(
@@ -53,7 +57,7 @@ def gdisconnect():
 def showCatalog():
     catalog = session.query(Catalog).order_by(asc(Catalog.name))
     #if 'username' not in login_session:
-    #    return render_template('publicrestaurants.html', restaurants=restaurants)
+    #    return render_template('publicCatalog.html', catalog=catalog)
     #else:
     return render_template('catalog.html', catalog=catalog)
 
@@ -61,31 +65,84 @@ def showCatalog():
 # Create a new category
 @app.route('/catalog/new/', methods=['GET', 'POST'])
 def newCatalog():
-    return 'page to enter new category'
+    #if 'username' not in login_session:
+    #    return redirect('/login')
+    if request.method == 'POST':
+        newCatalog = Catalog(
+            name=request.form['name'], user_id=login_session['user_id'])
+        session.add(newCatalog)
+        flash('New Team %s Successfully Created' % newCatalog.name)
+        session.commit()
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('newCatalog.html')
 
 
 # Edit a category
 @app.route('/catalog/<int:catalog_id>/edit/', methods=['GET', 'POST'])
 def editCatalog(catalog_id):
-    return 'edit category'
+    editedCatalog = session.query(
+        Catalog).filter_by(id=catalog_id).one()
+    #if 'username' not in login_session:
+    #    return redirect('/login')
+    if editedCatalog.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to edit this team. Please create your own team in order to edit.');}</script><body onload='myFunction()'>"
+    if request.method == 'POST':
+        if request.form['name']:
+            editedCatalog.name = request.form['name']
+            flash('Team Successfully Edited %s' % editedCatalog.name)
+            return redirect(url_for('showCatalog'))
+    else:
+        return render_template('editCatalog.html', catalog=editedCatalog)
 
 
 # Delete a category
 @app.route('/catalog/<int:catalog_id>/delete/', methods=['GET', 'POST'])
 def deleteCatalog(catalog_id):
-    return 'delete category'
+    catalogToDelete = session.query(
+        Catalog).filter_by(id=catalog_id).one()
+    #if 'username' not in login_session:
+    #    return redirect('/login')
+    if catalogToDelete.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to delete this team. Please create your own team in order to delete.');}</script><body onload='myFunction()'>"
+    if request.method == 'POST':
+        session.delete(catalogToDelete)
+        flash('%s Successfully Deleted' % catalogToDelete.name)
+        session.commit()
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('deleteCatalog.html', catalog=catalogToDelete)
 
 
 # Show a category details
 @app.route('/catalog/<int:catalog_id>/')
 def showCategory(catalog_id):
-    return 'show category details'
+    category = session.query(Catalog).filter_by(id=catalog_id).one()
+    creator = 'test' #getUserInfo(catalog.user_id)
+    items = session.query(CategoryItem).filter_by(
+        catalog_id=catalog_id).all()
+    #if 'username' not in login_session or creator.id != login_session['user_id']:
+    #    return render_template('publicCategoryItems.html', items=items, category=category, creator=creator)
+    #else:
+    return render_template('categoryItems.html', items=items, category=category, creator=creator)
 
 
 # Create a new category item
 @app.route('/catalog/<int:catalog_id>/new/', methods=['GET', 'POST'])
 def newCategoryItem(catalog_id):
-    return 'create new category item'
+    #if 'username' not in login_session:
+    #    return redirect('/login')
+    category = session.query(Catalog).filter_by(id=catalog_id).one()
+    if login_session['user_id'] != category.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to add players to this team. Please create your own team in order to add players.');}</script><body onload='myFunction()'>"
+        if request.method == 'POST':
+            newCategoryItem = CategoryItem(name=request.form['name'], description=request.form['description'], catalog_id=catalog_id, user_id=category.user_id)
+            session.add(newCategoryItem)
+            session.commit()
+            flash('New Player %s Successfully Created' % (newCategoryItem.name))
+            return redirect(url_for('showCategory', catalog_id=catalog_id))
+    else:
+        return render_template('newCategoryItem.html', catalog_id=catalog_id)
 
 
 # Edit a category item
